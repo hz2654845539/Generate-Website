0f26aeac10f839ca5c1fb44379c1d914a0f9b7a4
hz <2654845539@qq.com>
2026-06-28 17:08:10 +0800

    【新增】用户登录模块

---
+++ src/main/java/com/hz/hzaigeneratewebsite/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.hz.hzaigeneratewebsite.mapper;
+
+import com.hz.hzaigeneratewebsite.model.entity.User;
+import com.mybatisflex.core.BaseMapper;
+
+/**
+ * 用户 映射层。
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
